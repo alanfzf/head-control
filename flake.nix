e019d9b4a988eb2665b0c184f3700960abb4be06
alanfzf <29936954+alanfzf@users.noreply.github.com>
2026-09-04 16:33:09 -0600

    feat: basic scaffold

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A simple flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        devPackages = with nixpkgs; [
+          pkgs.nodejs
+        ];
+
+        shellHook = "";
+      in
+      {
+
+        devShells = {
+          default = pkgs.mkShell {
+            name = "dev-shell";
+            packages = devPackages;
+            shellHook = shellHook;
+          };
+        };
+      }
+    );
+}
